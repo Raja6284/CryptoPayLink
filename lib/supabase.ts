b8736a3092c26ef3a5d491c6edb748bb3be983a3
Raja Kumar <rajaraja360parth@gmail.com>
2025-07-14 11:22:57 +0530

    fixed the env import

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,8 +1,11 @@
 import { createClient } from '@supabase/supabase-js'
+//why it is not able to imort from .env.local?
+// Ensure that the environment variables are loaded
+
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-const supabaseServiceRoleKey =process.env.SUPABASE_SERVICE_ROLE_KEY
+const supabaseServiceRoleKey =process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY
 // Client-side Supabase client with error handling
 export const supabase = supabaseUrl && supabaseAnonKey 
   ? createClient(supabaseUrl, supabaseAnonKey)
